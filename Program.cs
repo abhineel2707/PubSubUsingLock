b1c9f39a4c3b44837b8befe34403e622a6123bab
Abhineel Rai <abhineel2707@gmail.com>
2020-06-17 19:44:54 +0530

    Added mutex logic

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace PubSubExample
         private static readonly object _lock = new object();
         const int BUFFER_CAPACITY = 10;
         const int TIMEOUT_IN_MILLISECONDS = 1000;
+        private static Mutex mutex;
         static void Main(string[] args)
         {
             BlockingQueue<int> blockingQueue = new BlockingQueue<int>(5);
@@ -36,36 +37,71 @@ namespace PubSubExample
             producerThread.Join();
             consumerThread.Join();
         }
+        // Using Mutex
         static void ProducerThread(BlockingQueue<int> blockingQueue, int timeOutInMilliseconds)
         {
-            lock (_lock)
+            mutex.WaitOne();
+            while (blockingQueue.IsFull)
             {
-                while (blockingQueue.IsFull)
-                {
-                    // lock is released and we are waiting
-                    Monitor.Wait(_lock);
-                }
+                // Release the mutex to give other threads a chance to acquire it
+                mutex.ReleaseMutex();
 
-                // After wait is completed we will reacquire the lock
-                int item = new Random(10).Next(100, 1000);
-                Console.WriteLine($"Inside producer thread. Writing {item} to the buffer queue.");
-                Console.WriteLine();
-                blockingQueue.Write(item);
+                // Reacquire the mutex before checking the condition
+                mutex.WaitOne();
             }
+
+            // After wait is completed we will reacquire the lock
+            int item = new Random(10).Next(100, 1000);
+            Console.WriteLine($"Inside producer thread. Writing {item} to the buffer queue.");
+            Console.WriteLine();
+            blockingQueue.Write(item);
+            mutex.ReleaseMutex();
         }
         static void ConsumerThread(BlockingQueue<int> blockingQueue, int timeOutInMilliseconds)
         {
-            lock (_lock)
+            mutex.WaitOne();
+            while (!blockingQueue.IsFull)
             {
-                while (!blockingQueue.IsFull)
-                {
-                    Monitor.Pulse(_lock);
-                }
-                int item = blockingQueue.Read();
-                Console.WriteLine($"Inside consumer thread.");
-                Console.WriteLine($"Read {item} from buffer queue");
-                Console.WriteLine();
+                mutex.ReleaseMutex();
+
+                mutex.WaitOne();
             }
+            int item = blockingQueue.Read();
+            Console.WriteLine($"Inside consumer thread.");
+            Console.WriteLine($"Read {item} from buffer queue");
+            Console.WriteLine();
+            mutex.ReleaseMutex();
         }
+        // static void ProducerThread(BlockingQueue<int> blockingQueue, int timeOutInMilliseconds)
+        // {
+        //     lock (_lock)
+        //     {
+        //         while (blockingQueue.IsFull)
+        //         {
+        //             // lock is released and we are waiting
+        //             Monitor.Wait(_lock);
+        //         }
+
+        //         // After wait is completed we will reacquire the lock
+        //         int item = new Random(10).Next(100, 1000);
+        //         Console.WriteLine($"Inside producer thread. Writing {item} to the buffer queue.");
+        //         Console.WriteLine();
+        //         blockingQueue.Write(item);
+        //     }
+        // }
+        // static void ConsumerThread(BlockingQueue<int> blockingQueue, int timeOutInMilliseconds)
+        // {
+        //     lock (_lock)
+        //     {
+        //         while (!blockingQueue.IsFull)
+        //         {
+        //             Monitor.Pulse(_lock);
+        //         }
+        //         int item = blockingQueue.Read();
+        //         Console.WriteLine($"Inside consumer thread.");
+        //         Console.WriteLine($"Read {item} from buffer queue");
+        //         Console.WriteLine();
+        //     }
+        // }
     }
 }
